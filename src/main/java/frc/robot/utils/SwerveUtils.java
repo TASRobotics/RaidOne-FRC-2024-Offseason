bfc5746a03ba9f656b2c49c653194675e36244bf
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-09-23 20:34:18 +0800

    Updated parameter naming scheme

--- src/main/java/frc/robot/utils/SwerveUtils.java
+++ src/main/java/frc/robot/utils/SwerveUtils.java
@@ -3,86 +3,86 @@ package frc.robot.utils;
 public class SwerveUtils {
     /**
      * Steps a value towards a target with a specified step size.
-     * @param _current The current or starting value.  Can be positive or negative.
-     * @param _target The target value the algorithm will step towards.  Can be positive or negative.
-     * @param _stepsize The maximum step size that can be taken.
-     * @return The new value for {@code _current} after performing the specified step towards the specified target.
+     * @param iCurrent The current or starting value.  Can be positive or negative.
+     * @param iTarget The target value the algorithm will step towards.  Can be positive or negative.
+     * @param iStepsize The maximum step size that can be taken.
+     * @return The new value for {@code iCurrent} after performing the specified step towards the specified target.
      */
-    public static double StepTowards(double _current, double _target, double _stepsize) {
-        if (Math.abs(_current - _target) <= _stepsize) {
-            return _target;
+    public static double StepTowards(double iCurrent, double iTarget, double iStepsize) {
+        if (Math.abs(iCurrent - iTarget) <= iStepsize) {
+            return iTarget;
         }
-        else if (_target < _current) {
-            return _current - _stepsize;
+        else if (iTarget < iCurrent) {
+            return iCurrent - iStepsize;
         }
         else {
-            return _current + _stepsize;
+            return iCurrent + iStepsize;
         }
     }
 
     /**
      * Steps a value (angle) towards a target (angle) taking the shortest path with a specified step size.
-     * @param _current The current or starting angle (in radians).  Can lie outside the 0 to 2*PI range.
-     * @param _target The target angle (in radians) the algorithm will step towards.  Can lie outside the 0 to 2*PI range.
-     * @param _stepsize The maximum step size that can be taken (in radians).
-     * @return The new angle (in radians) for {@code _current} after performing the specified step towards the specified target.
+     * @param iCurrent The current or starting angle (in radians).  Can lie outside the 0 to 2*PI range.
+     * @param iTarget The target angle (in radians) the algorithm will step towards.  Can lie outside the 0 to 2*PI range.
+     * @param iStepsize The maximum step size that can be taken (in radians).
+     * @return The new angle (in radians) for {@code iCurrent} after performing the specified step towards the specified target.
      * This value will always lie in the range 0 to 2*PI (exclusive).
      */
-    public static double StepTowardsCircular(double _current, double _target, double _stepsize) {
-        _current = WrapAngle(_current);
-        _target = WrapAngle(_target);
+    public static double StepTowardsCircular(double iCurrent, double iTarget, double iStepsize) {
+        iCurrent = WrapAngle(iCurrent);
+        iTarget = WrapAngle(iTarget);
 
-        double stepDirection = Math.signum(_target - _current);
-        double difference = Math.abs(_current - _target);
+        double stepDirection = Math.signum(iTarget - iCurrent);
+        double difference = Math.abs(iCurrent - iTarget);
         
-        if (difference <= _stepsize) {
-            return _target;
+        if (difference <= iStepsize) {
+            return iTarget;
         }
         else if (difference > Math.PI) { //does the system need to wrap over eventually?
             //handle the special case where you can reach the target in one step while also wrapping
-            if (_current + 2*Math.PI - _target < _stepsize || _target + 2*Math.PI - _current < _stepsize) {
-                return _target;
+            if (iCurrent + 2*Math.PI - iTarget < iStepsize || iTarget + 2*Math.PI - iCurrent < iStepsize) {
+                return iTarget;
             }
             else {
-                return WrapAngle(_current - stepDirection * _stepsize); //this will handle wrapping gracefully
+                return WrapAngle(iCurrent - stepDirection * iStepsize); //this will handle wrapping gracefully
             }
 
         }
         else {
-            return _current + stepDirection * _stepsize;
+            return iCurrent + stepDirection * iStepsize;
         }
     }
 
     /**
      * Finds the (unsigned) minimum difference between two angles including calculating across 0.
-     * @param _angleA An angle (in radians).
-     * @param _angleB An angle (in radians).
+     * @param iAngleA An angle (in radians).
+     * @param iAngleB An angle (in radians).
      * @return The (unsigned) minimum difference between the two angles (in radians).
      */
-    public static double AngleDifference(double _angleA, double _angleB) {
-        double difference = Math.abs(_angleA - _angleB);
+    public static double AngleDifference(double iAngleA, double iAngleB) {
+        double difference = Math.abs(iAngleA - iAngleB);
         return difference > Math.PI? (2 * Math.PI) - difference : difference;
     }
 
     /**
      * Wraps an angle until it lies within the range from 0 to 2*PI (exclusive).
-     * @param _angle The angle (in radians) to wrap.  Can be positive or negative and can lie multiple wraps outside the output range.
+     * @param iAngle The angle (in radians) to wrap.  Can be positive or negative and can lie multiple wraps outside the output range.
      * @return An angle (in radians) from 0 and 2*PI (exclusive).
      */
-    public static double WrapAngle(double _angle) {
+    public static double WrapAngle(double iAngle) {
         double twoPi = 2*Math.PI;
 
-        if (_angle == twoPi) { // Handle this case separately to avoid floating point errors with the floor after the division in the case below
+        if (iAngle == twoPi) { // Handle this case separately to avoid floating point errors with the floor after the division in the case below
             return 0.0;
         }
-        else if (_angle > twoPi) {
-            return _angle - twoPi*Math.floor(_angle / twoPi);
+        else if (iAngle > twoPi) {
+            return iAngle - twoPi*Math.floor(iAngle / twoPi);
         }
-        else if (_angle < 0.0) {
-            return _angle + twoPi*(Math.floor((-_angle) / twoPi)+1);
+        else if (iAngle < 0.0) {
+            return iAngle + twoPi*(Math.floor((-iAngle) / twoPi)+1);
         }
         else {
-            return _angle;
+            return iAngle;
         }
     }
 }
