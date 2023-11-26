df981584a98c599234f2f56f3ebd47899c3d36d9
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-11-26 20:35:49 +0800

    Created auto command sequence

---
+++ src/main/java/frc/robot/commands/auto/ForwardPath.java
@@ -0,0 +1,38 @@
+package frc.robot.commands.auto;
+
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.DrivePath;
+
+public class ForwardPath extends CommandBase {
+
+    private final Trajectory path1 = PathPlanner.loadPath("TestPath1", AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared);
+    
+    public ForwardPath() {};
+
+    @Override
+    public void initialize() {
+        SequentialCommandGroup command = new SequentialCommandGroup(
+            new DrivePath(RobotContainer.getSwerve(), path1)
+        );
+
+        command.schedule();
+    }
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
