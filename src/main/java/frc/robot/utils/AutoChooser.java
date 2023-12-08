9a9f599fdc65047a944604429036e5936a3797aa
0x5b62656e5d <112295217+0x5b62656e5d@users.noreply.github.com>
2023-12-08 19:06:24 +0800

    Refactored code

--- src/main/java/frc/robot/utils/AutoChooser.java
+++ src/main/java/frc/robot/utils/AutoChooser.java
@@ -8,7 +8,6 @@ import frc.robot.commands.auto.*;
 public class AutoChooser {
 
     private SendableChooser<CommandBase> chooser;
-    private CommandBase selectedCommand;
 
     // List of commands for autonomous
     private CommandBase[] commands = {
@@ -21,7 +20,7 @@ public class AutoChooser {
      */
     public AutoChooser() {
         chooser = new SendableChooser<>();
-        chooser.setDefaultOption("Empty sequence", commands[0]);
+        chooser.setDefaultOption("EmptyAuto", commands[0]);
         for(CommandBase cmd : commands){
             chooser.addOption(cmd.getName(), cmd);
         }
@@ -29,22 +28,6 @@ public class AutoChooser {
                 .add("Auton Selection", chooser)
                 .withSize(3, 1)
                 .withPosition(2, 1);
-    }
-
-    /**
-     * Reads the selected autonomous command from the SendableChooser.
-     */
-    public void readCommand() {
-        selectCommand(chooser.getSelected());
-    }
-
-    /**
-     * Selects an autonomous command to run.
-     * 
-     * @param iComand The autonomous command to run
-     */
-    public void selectCommand(CommandBase iCommand) {
-        selectedCommand = iCommand;
     }
 
     /**
@@ -56,7 +39,7 @@ public class AutoChooser {
      * @return Currently selected autonomous command
      */
     public CommandBase getSelectedCommand() {
-        return selectedCommand;
+        return chooser.getSelected();
     }
     
 }
